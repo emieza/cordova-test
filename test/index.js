503a863995864d806dd97b54cac624360e389371
Enric Mieza <emieza@xtec.cat>
2023-12-01 19:46:04 +0100

    child process

--- test/index.js
+++ test/index.js
@@ -1,18 +1,50 @@
 const {Builder, Browser, By, Key, until} = require("selenium-webdriver");
 const firefox = require('selenium-webdriver/firefox');
-var assert = require('assert');
-
+const chrome = require('selenium-webdriver/chrome');
+const { exec } = require("child_process");
+const assert = require('assert');
 const HEADLESS = true;
+
+const { spawn } = require("child_process");
+
+
+// Engeguem server amb la APP
+const cmd = spawn("cordova", ["serve"]);
+
+cmd.stdout.on("data", data => {
+    console.log(`stdout: ${data}`);
+});
+
+cmd.stderr.on("data", data => {
+    console.log(`stderr: ${data}`);
+});
+
+cmd.on('error', (error) => {
+    console.log(`error: ${error.message}`);
+});
+
+cmd.on("close", code => {
+    console.log(`child process exited with code ${code}`);
+});
+
+
+// TESTS
 
 (async function test_exemple() {
     // Configurem driver
     let driver = await new Builder()
-            .forBrowser(Browser.FIREFOX)
-            .setFirefoxOptions(new firefox.Options().headless())
+            //.forBrowser(Browser.FIREFOX)
+            //.setFirefoxOptions(new firefox.Options().headless())
+            .forBrowser(Browser.CHROME)
+            //.setChromeOptions(new chrome.Options().addArguments('--headless=new'))
             .build();
     try {
-        await driver.get("http://localhost:8000/");
-        await driver.findElement(By.name("usuari")).sendKeys("webdriver", Key.RETURN);
+        // deixem temps a que el servidor es posi en marxa
+        await driver.sleep(2000);
+
+        // testejem
+        await driver.get("http://localhost:8000/browser/www/");
+        await driver.findElement(By.name("usuari")).sendKeys("pepa", Key.RETURN);
         await driver.findElement(By.tagName("button")).click();
 
         // alert selenium doc: https://www.selenium.dev/documentation/webdriver/interactions/alerts/
@@ -26,7 +58,11 @@ const HEADLESS = true;
 
         //await driver.wait(until.titleIs("webdriver — Google Search"), 4000);
         await driver.sleep(2000);
+
     } finally {
+        // tanquem servidor
+        await cmd.kill("SIGHUP")
+        // tanquem browser
         await driver.quit();
     }
 
